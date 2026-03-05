ef280987799b45749636631c9c0d773838173862
Soham Chopdar <129845535+SohamChopdar@users.noreply.github.com>
2026-03-05 13:28:46 +0530

    Add files via upload
    setup project structure and database connection

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "campus_support_system";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if(!$conn){
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
