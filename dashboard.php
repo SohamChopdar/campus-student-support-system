764f748dc5e8ce2526163951341736faf5821b24
Soham Chopdar <129845535+SohamChopdar@users.noreply.github.com>
2026-03-07 14:22:12 +0530

    Add files via upload
    displayed available services on student dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "../config/db.php";
 
 if(!isset($_SESSION['user_id'])){
     header("Location: login.php");
@@ -9,6 +10,28 @@ if(!isset($_SESSION['user_id'])){
 
 <h2>Welcome <?php echo $_SESSION['name']; ?>!</h2>
 
-<p>You are logged in.</p>
+<h3>Available Services</h3>
+
+<?php
+
+$sql = "SELECT * FROM services";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0){
+
+    while($service = mysqli_fetch_assoc($result)){
+        
+        echo "<div>";
+        echo "<h4>".$service['service_name']."</h4>";
+        echo "<p>".$service['description']."</p>";
+        echo "</div>";
+        echo "<hr>";
+    }
+
+}else{
+    echo "No services available.";
+}
+
+?>
 
 <a href="logout.php">Logout</a>
